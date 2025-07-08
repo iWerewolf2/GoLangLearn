8fcefb767000e64b3003ab7ef13581861134cdf8
Daniil <daniil.kachan71@gmail.com>
2025-07-08 20:41:43 +0200

    New http page

--- GoExtended/NewLesson/main.go
+++ GoExtended/NewLesson/main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 )
 
-func home_page(w http.ResponseWriter, r *http.Request) {
+func home_page(page http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(page, "My http page")
 
 }
 
