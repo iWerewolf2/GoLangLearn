308954f6d84325538e133a00f390490f11f95655
Daniil Kachan <67536563+iWerewolf2@users.noreply.github.com>
2025-07-16 20:03:47 +0200

    Update repo.go

--- GoExtended/API/repo.go
+++ GoExtended/API/repo.go
@@ -1,41 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-var currentId int
-
-var todos Todos
-
-// Give us some seed data
-func init() {
-	RepoCreateTodo(Todo{Name: "Write presentation"})
-	RepoCreateTodo(Todo{Name: "Host meetup"})
+type Todo struct {
+	Id   int
+	Name string
 }
 
-func RepoFindTodo(id int) Todo {
-	for _, t := range todos {
+type Todos []Todo
+
+type TodoRepo struct {
+	mu        sync.Mutex
+	currentId int
+	todos     Todos
+}
+
+// NewTodoRepo initializes the repo with seed data
+func NewTodoRepo() *TodoRepo {
+	repo := &TodoRepo{}
+	repo.CreateTodo(Todo{Name: "Write presentation"})
+	repo.CreateTodo(Todo{Name: "Host meetup"})
+	return repo
+}
+
+func (r *TodoRepo) FindTodo(id int) (Todo, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	for _, t := range r.todos {
 		if t.Id == id {
-			return t
+			return t, nil
 		}
 	}
-	// return empty Todo if not found
-	return Todo{}
+	return Todo{}, fmt.Errorf("Todo with id %d not found", id)
 }
 
-// this is bad, I don't think it passes race condtions
-func RepoCreateTodo(t Todo) Todo {
-	currentId += 1
-	t.Id = currentId
-	todos = append(todos, t)
+func (r *TodoRepo) CreateTodo(t Todo) Todo {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.currentId++
+	t.Id = r.currentId
+	r.todos = append(r.todos, t)
 	return t
 }
 
-func RepoDestroyTodo(id int) error {
-	for i, t := range todos {
+func (r *TodoRepo) DestroyTodo(id int) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	for i, t := range r.todos {
 		if t.Id == id {
-			todos = append(todos[:i], todos[i+1:]...)
+			r.todos = append(r.todos[:i], r.todos[i+1:]...)
 			return nil
 		}
 	}
-	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+	return fmt.Errorf("Could not find Todo with id %d to delete", id)
 }
